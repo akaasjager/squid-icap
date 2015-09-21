0ea971df4dd5195ca8b27d97638d4bdd85a09fc5
Alexander Kaasjager (SBP) <akaasjager@schubergphilis.com>
2015-09-21 18:41:49 +0200

    Metadata to reflect ICAP capabilities

--- metadata.rb
+++ metadata.rb
@@ -1,16 +1,16 @@
-name             'squid'
-maintainer       'Chef Software, Inc.'
-maintainer_email 'cookbooks@chef.io'
+name             'squid-icap'
+maintainer       '@akaasjager'
+maintainer_email 'cookbooks@kalex.nl'
 license          'Apache 2.0'
-description      'Installs/configures squid as a simple caching proxy'
+description      'Installs/configures squid as a simple caching proxy - with ICAP support'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.1.0'
+version          '1.1.1'
 
-%w(debian ubuntu centos fedora redhat scientific suse amazon smartos freebsd).each do |os|
+%w(centos fedora redhat scientific).each do |os|
   supports os
 end
 
-recipe 'squid::default', 'Installs and configures Squid.'
+recipe 'squid::default', 'Installs and configures Squid with ICAP.'
 
-source_url 'https://github.com/chef-cookbooks/squid' if respond_to?(:source_url)
-issues_url 'https://github.com/chef-cookbooks/squid/issues' if respond_to?(:issues_url)
+source_url 'https://github.com/akaasjager/squid-icap' if respond_to?(:source_url)
+issues_url 'https://github.com/akaasjager/squid-icap/issues' if respond_to?(:issues_url)
